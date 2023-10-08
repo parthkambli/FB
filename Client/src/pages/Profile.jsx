95b2168269edb139a58afefc410d60aa14880b42
Parth <parthkambli.pk1@gmail.com>
2023-10-08 17:22:31 +0530

    Edit profile form

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,8 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useContext, useState } from "react";
 import ProfilePicture from "../assets/Profile.png";
 import RecipeCard from "../components/RecipeCard";
+import { AuthContext } from "../context/Auth/AuthContext";
 
 const Profile = () => {
+  const { user } = useContext(AuthContext);
+
+  const [name, setName] = useState(user.Full_Name || "");
+  const [userName, setUserName] = useState(user.User_Name || "");
+  const [bio, setBio] = useState(user.Bio || "");
+
   return (
     <div className="container">
       <div className="p-2 my-2" style={{ backgroundColor: "#F1F1F1" }}>
@@ -30,10 +38,120 @@ const Profile = () => {
               <button
                 type="button"
                 className="btn btn-sm rounded-pill m-2"
+                onClick={console.log("user:-", user)}
+                data-bs-toggle="modal"
+                data-bs-target="#editProfile"
                 style={{ backgroundColor: "#FC7300", color: "#F1F1F1" }}
               >
                 Edit Profile
               </button>
+              {/* Modal  */}
+              <div
+                className="modal fade"
+                id="editProfile"
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h1 className="modal-title fs-5" id="exampleModalLabel">
+                        Edit Profile
+                      </h1>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      <form className="p-4">
+                        <div className="mb-3 row">
+                          <label
+                            htmlFor="ProfilePicture"
+                            className="col-3 col-form-label"
+                          >
+                            Profile Picture:-
+                          </label>
+                          <div className="col-9">
+                            <input
+                              type="file"
+                              className="form-control"
+                              id="ProfilePicture"
+                            />
+                          </div>
+                        </div>
+                        <div className="mb-3 row">
+                          <label
+                            htmlFor="Name"
+                            className="col-3 col-form-label"
+                          >
+                            Name:-
+                          </label>
+                          <div className="col-9">
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="Name"
+                              value={name}
+                              onChange={(e) => setName(e.target.value)}
+                            />
+                          </div>
+                        </div>
+                        <div className="mb-3 row">
+                          <label
+                            htmlFor="UserName"
+                            className="col-3 col-form-label"
+                          >
+                            User Name: -
+                          </label>
+                          <div className="col-9">
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="UserName"
+                              value={userName}
+                              onChange={(e) => setUserName(e.target.value)}
+                            />
+                          </div>
+                        </div>
+                        <div className="mb-3 row">
+                          <label htmlFor="Bio" className="col-3 col-form-label">
+                            Bio: -
+                          </label>
+                          <div className="col-9">
+                            <textarea
+                              className="form-control"
+                              id="Bio"
+                              rows="3"
+                              value={bio}
+                              onChange={(e) => setBio(e.target.value)}
+                            ></textarea>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                      <button
+                        type="button"
+                        className="btn"
+                        style={{ backgroundColor: "#00425A", color: "#F1F1F1" }}
+                      >
+                        Edit
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <button
                 type="button"
                 className="btn btn-sm rounded-pill m-2"
